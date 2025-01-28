331f7849d08a4bc7e5fe5d9ec01e24262b5b54ed
Atreyee <112842793+River-unknown@users.noreply.github.com>
2025-01-28 13:33:13 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -8,13 +8,23 @@ db_conn_String = os.environ["DB_CONNECTION_STRING"]
 engine = create_engine(db_conn_String
 , connect_args={"ssl": {"ssl_ca": os.environ["CA_CERTIFICATE"]}}
 )
+
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-
-
   jobs = []
   for row in result.all():
     jobs.append(row._mapping)
 
   return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{"val":id})
+    rows = result.all()
+    if len(rows)==0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
